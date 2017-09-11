4601dd5c9224eea5eea9e4fbd0eca1b9f4fa24c7
Tchase44 <tchase4544@gmail.com>
2017-09-11 18:23:15 -0400

    angular templating

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,26 @@
 (function(){
-	
+	angular.module("timbr",[
+		"ui.router",
+		"ngResource"
+	])
+	.config([
+		"$stateProvider",
+		"$locationProvider",
+		routes
+	])
+	.controller(,[])
+/*
+*/
+function routes(){
+	$locationProvider.html5Mode(true);
+	$stateProvider
+	.state("home",
+		url: "/",
+		templateUrl: "js/ng-views/",
+		controller: "",
+		controllerAs: ""
+	)
+}
+
+
 })()
