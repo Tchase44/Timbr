74a78626d9966a8c018175a683d03cbabc759846
Tchase44 <tchase4544@gmail.com>
2017-10-31 09:57:09 -0400

    basic vue setup before npm install

---
+++ src/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="landing">
+  	<h1>Welcome to Timbr Co.</h1>
+	<h2>One Place For</h2>
+	<h2>All Your Woodworking Needs</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+  // components: {}
+}
+</script>
+
+<style>
+
+</style>
