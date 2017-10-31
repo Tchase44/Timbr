d187e147635b228ccb62c88ec93e443624e93bd3
Tchase44 <tchase4544@gmail.com>
2017-10-31 08:14:33 -0400

    updated mongo schema

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,18 @@
 const mongoose = require("mongoose")
 
-let DealerSchema = mongoose.Schema({})
+let DealerSchema = mongoose.Schema({
+                companyID: Number,
+                name: String,
+                phone1: String,
+                email: String,
+                addressLine1: String,
+                addressLine2:String,
+                city: String,
+                state: String,
+                country: String,
+                zipcode: Number,
+                weekHours: Object
+})
 
 mongoose.model("Shop",DealerSchema)
 mongoose.connect("mongodb:/localhost/timbr")
