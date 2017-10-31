c276c8aaa5d2546e4c3798310450773439f5b830
Tchase44 <tchase4544@gmail.com>
2017-10-31 09:26:55 -0400

    clear comments

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// const location = {
-//     "zipcode": "28205",
-//     "location": {
-//         "lat": 35.2263714,
-//         "lon": -80.7990185
-//     }
+
 const mongoose = require('./db/connection.js')
 const express = require('express')
 
